adec2f180db8d82365c243c9cf7c830d8eec9dac
Luiz Fernando <lfernando-silva@hotmail.com>
2017-01-13 14:48:27 -0200

    Update RequestHandler.js

--- handlers/RequestHandler.js
+++ handlers/RequestHandler.js
@@ -1,17 +1,9 @@
-﻿const EDL = require('easy-dependency-loader')
+const EDL = require('easy-dependency-loader')
 const Promise = EDL.load('bluebird')
-const path = EDL.load('path')
-const fs = Promise.promisifyAll(EDL.load('fs'))
-
-const filesPath = './files'
 
 module.exports = {
     handle: (request) => {
-        return Promise
-            .resolve(path.join(filesPath, request.url))
-            .then(fs.readFileAsync)
-            .catch(function (err) {
-                return 'ERRO PORQUE NÃO EXISTE O ARQUIVO';
-            });
+        //personalizar request
+        return request;
     }
 }
