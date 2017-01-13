5de886957332fc6b6021361dd0965538acea4295
Luiz Fernando <lfernando-silva@hotmail.com>
2017-01-13 14:47:20 -0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+﻿const EDL = require('easy-dependency-loader');
+
+EDL.setDependencies({
+    "RequestHandler": "./handlers/RequestHandler.js"
+});
+
+const http = EDL.load('http')
+const RequestHandler = EDL.load('RequestHandler');
+const Promise = require('bluebird');
+
+const port = 3000
+const host = '192.168.0.123'
+
+const requestHandler = (request, response) => {
+    return Promise
+        .resolve(request)
+        .then(RequestHandler.handle)
+        .then(function (data) {
+            response.write(data.toString())
+            return response.end();
+        });  
+}
+
+const listenCb = (err) => {
+    if (err) return console.log('something bad happened', err)
+    console.log('server is listening on ' + port)
+}
+
+http
+    .createServer(requestHandler)
+    .listen(port, host, listenCb)
