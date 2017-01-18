554d42bb71ea3352eac1020a68d6763390b39cc9
Luiz Fernando <lfernando-silva@hotmail.com>
2017-01-18 18:48:00 -0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# server-http-fast
-Servidor http rápido. Operações devem ficar no request handler.
+﻿# ServerHttp
+
+
--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const http = EDL.load('http')
 const RequestHandler = EDL.load('RequestHandler');
 const Promise = require('bluebird');
 
-const port = process.env.PORT || 3000
-const host = '192.168.0.123'
+const port = process.env.PORT || 80
+const host = '0.0.0.0'
 
 const requestHandler = (request, response) => {
     return Promise
         .resolve(request)
         .then(RequestHandler.handle)
         .then(function (data) {
+            response.removeHeader('Connection');
+            response.removeHeader('Date');
+            response.removeHeader('Transfer-Encoding');
             response.write(data.toString())
             return response.end();
         });  
